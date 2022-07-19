67086efb1276ff45010611ec4f8aa78eed310f95
qosole <williamj99wei@gmail.com>
2022-07-19 15:58:02 -0500

    Add comments to Letter.js

--- Develop/lib/Letter.js
+++ Develop/lib/Letter.js
@@ -4,6 +4,9 @@ class Letter {
     const digitList = '1234567890';
 
     this.char = letter;
+    // Visible indicated whether the letter should be visible
+    // Letters that have been guessed will be visible
+    // Letters that are not in letterList or digitList are automatically visible
     this.visible = false;
     if (letterList.indexOf(this.char) == -1 && digitList.indexOf(this.char) == -1) {
       this.visible = true;
@@ -11,6 +14,7 @@ class Letter {
   }
 
   toString() {
+    // Overriding toString() to return the letter only if it is visible 
     if (this.visible) {
         return this.char;
     }
@@ -18,6 +22,7 @@ class Letter {
   }
 
   guess(guess) {
+    // If the user's guess is correct, makes the letter visible and returns true
     if (this.char == guess) {
       this.visible = true;
       return true;
